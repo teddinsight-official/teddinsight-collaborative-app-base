1250eb94c1151a5be85c0869e128109ed8d73c3b
Oluwatayo <oluwatayoadegboye@gmail.com>
2019-01-29 14:51:53 +0100

    wip

--- app/src/main/java/ng/com/teddinsight/teddinsightbase/MainActivity.java
+++ app/src/main/java/ng/com/teddinsight/teddinsightbase/MainActivity.java
@@ -1,17 +1,11 @@
 package ng.com.teddinsight.teddinsightbase;
 
-import androidx.appcompat.app.AppCompatActivity;
-import ng.com.teddinsight.teddinsightchat.StaffsListFragment;
-import ng.com.teddinsight.teddinsightchat.ThreadFragment;
-import ng.com.teddinsight.teddinsightchat.models.User;
-import ng.com.teddinsight.teddinsightchat.listeners.Listeners;
-
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements Listeners.StaffItemListener {
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,25 +13,25 @@ public class MainActivity extends AppCompatActivity implements Listeners.StaffIt
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-            FirebaseAuth.getInstance().signInWithEmailAndPassword("creative.designer@teddinsight.com", "donmickey").addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content, StaffsListFragment.NewInstance()).commit();
-                } else {
-                    Toast.makeText(MainActivity.this, "login failed " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            getSupportFragmentManager().beginTransaction().replace(R.id.content, StaffsListFragment.NewInstance()).commit();
-        }
-
-    }
-
-    @Override
-    public void onStaffItemClicked(User user) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.content, ThreadFragment.NewInstance(user)).addToBackStack(null).commit();
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+//            FirebaseAuth.getInstance().signInWithEmailAndPassword("creative.designer@teddinsight.com", "donmickey").addOnCompleteListener(task -> {
+//                if (task.isSuccessful()) {
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.content, StaffsListFragment.NewInstance()).commit();
+//                } else {
+//                    Toast.makeText(MainActivity.this, "login failed " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        } else {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.content, StaffsListFragment.NewInstance()).commit();
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onStaffItemClicked(User user) {
+//        getSupportFragmentManager().beginTransaction().replace(R.id.content, ThreadFragment.NewInstance(user)).addToBackStack(null).commit();
+//    }
 }
