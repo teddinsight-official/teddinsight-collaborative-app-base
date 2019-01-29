dd2d7825e980f42d04e7fe515131a2f182256f2a
Oluwatayo <oluwatayoadegboye@gmail.com>
2019-01-29 08:35:56 +0100

    remove sample files

--- app/src/main/java/ng/com/teddinsight/teddinsightbase/MainActivity.java
+++ app/src/main/java/ng/com/teddinsight/teddinsightbase/MainActivity.java
@@ -1,17 +1,10 @@
 package ng.com.teddinsight.teddinsightbase;
 
-import androidx.appcompat.app.AppCompatActivity;
-import ng.com.teddinsight.teddinsightchat.StaffsListFragment;
-import ng.com.teddinsight.teddinsightchat.ThreadFragment;
-import ng.com.teddinsight.teddinsightchat.User;
-import ng.com.teddinsight.teddinsightchat.listeners.Listeners;
-
 import android.os.Bundle;
-import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements Listeners.StaffItemListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +15,5 @@ public class MainActivity extends AppCompatActivity implements Listeners.StaffIt
     @Override
     protected void onStart() {
         super.onStart();
-        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
-            FirebaseAuth.getInstance().signInWithEmailAndPassword("creative.designer@teddinsight.com", "donmickey").addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.content, StaffsListFragment.NewInstance()).commit();
-                } else {
-                    Toast.makeText(MainActivity.this, "login failed " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            getSupportFragmentManager().beginTransaction().replace(R.id.content, StaffsListFragment.NewInstance()).commit();
-        }
-
-    }
-
-    @Override
-    public void onStaffItemClicked(User user) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.content, ThreadFragment.NewInstance(user)).addToBackStack(null).commit();
     }
 }
